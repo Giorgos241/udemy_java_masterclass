a4946c5ed872a56ba47538fc6dd1261b7a5737d4
Giorgos241 <georgegoumas@gmail.com>
2023-02-13 00:16:39 +0200

    working solution for leap year problem

--- Javamasterclass/src/Main.java
+++ Javamasterclass/src/Main.java
@@ -1,23 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(shouldWakeUp(true, 0));
-    }
+        System.out.println(isLeapYear(1855));
+      }
+
     /*
-    * We have a dog that likes to bark.
-    * We need to wake up if the dog is barking at night!
-    * Write a method shouldWakeUp that has 2 parameters.
-    * 1st parameter should be of type boolean and be named barking it represents if our dog is currently barking.
-    * 2nd parameter represents the hour of the day and is of type int with the name hourOfDay and has a valid range of 0-23.
-    * We have to wake up if the dog is barking before 8 or after 22 hours so in that case return true.
-    */
-    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
-        if(barking){
-            //check time is of valid range of 0-23
-            if(hourOfDay >= 0 && hourOfDay <= 23) {
-                //check if we should make the dog stop barking
-                return (hourOfDay < 8 || hourOfDay > 22) ? true : false;
+     * Write a method isLeapYear with a parameter of type int named year.
+     * The parameter needs to be greater than or equal to 1 and less than or equal to 9999.
+     * If the parameter is not in that range return false.
+     * Otherwise, if it is in the valid range, calculate if the year is a leap year and return true
+     * if it is a leap year, otherwise return false.
+     * */
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if ((year % 4 == 0) && (year % 100 != 0)) {
+                return true;
+            }else if((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)){
+                return true;
             }
-        }
+            }else{
+                return false;
+            }
         return false;
     }
 }
