e5ce168c3863d26ae2b3e94d1b3a81b47f02f63a
Giorgos241 <georgegoumas@gmail.com>
2023-02-12 22:34:39 +0200

    simple solution

--- Javamasterclass/src/Main.java
+++ Javamasterclass/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(shouldWakeUp(true, -1));
+        System.out.println(shouldWakeUp(true, 0));
     }
     /*
     * We have a dog that likes to bark.
@@ -11,27 +11,13 @@ public class Main {
     * We have to wake up if the dog is barking before 8 or after 22 hours so in that case return true.
     */
     public static boolean shouldWakeUp(boolean barking, int hourOfDay){
-        //check for barking and check if the hours are between the accepted range (0-23)
-        return isTheDogBarking(barking) && checkHoursOfTheDay(hourOfDay) && checkIfWeShouldWakeUpDog(hourOfDay);
-    }
-
-    private static boolean isTheDogBarking(boolean barking) {
-        return barking ? barking : false;
-    }
-
-    /**
-     *
-     * @param hourOfDay a int that we would like to be between 0-23
-     * @return boolean value that will be TRUE if we are between the acceptable range.
-     * FALSE if it is lower or greater than our range
-     */
-    private static boolean checkHoursOfTheDay(int hourOfDay) {
-        /*We check that the hourOfDay is between the acceptable range*/
-        return (hourOfDay > -1 && hourOfDay < 24) ? checkIfWeShouldWakeUpDog(hourOfDay) : false;
-    }
-
-    private static boolean checkIfWeShouldWakeUpDog(int hourOfDay) {
-        /*We have to wake up if the dog is barking before 8 or after 22 hours so in that case return true*/
-        return (hourOfDay < 8 || hourOfDay > 22) ? true : false;
+        if(barking){
+            //check time is of valid range of 0-23
+            if(hourOfDay >= 0 && hourOfDay <= 23) {
+                //check if we should make the dog stop barking
+                return (hourOfDay < 8 || hourOfDay > 22) ? true : false;
+            }
+        }
+        return false;
     }
 }
