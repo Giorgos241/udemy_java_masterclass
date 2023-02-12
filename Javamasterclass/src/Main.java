ad692a911adb7b9f7f15ba21a890ce4b7a102c80
George <george@george-macbook-air.home>
2023-02-12 15:48:39 +0200

    First commit

---
+++ Javamasterclass/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * Hello world! Do checkout each my (accepted) solution to the Udemy course "Java Programming Masterclass updated to Java 17"
+        *
+        * I tried creating a branch for each exercise, so you can test out the solution yourselves before submitting it.
+        * Do note, some solutions will not be accepted, but I kept them anyway since they do actually work, but are not accepted as solutions by Udemy :(
+        * 
+        */
+    }
+}
