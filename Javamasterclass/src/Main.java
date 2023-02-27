d15a10c4dafffa3feacfa9c384a1f111295bb3d4
Giorgos241 <georgegoumas@gmail.com>
2023-02-27 17:54:49 +0200

    AreaCalculator

--- Javamasterclass/src/Main.java
+++ Javamasterclass/src/Main.java
@@ -1,29 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(hasTeen(9, 99,19));
-        System.out.println(isTeen(13));
-      }
-
-    /*
-     * The method should return boolean, and it needs to return true if one of the parameters is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
-     */
-
-    /*
-    * Write another method named isTeen with 1 parameter of type int.
-    * The method should return boolean and it needs to return true if the parameter is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
-    */
-
-    public static boolean hasTeen(int numberOne, int numberTwo, int numberThree) {
-        int[] numberArray = {numberOne,numberTwo,numberThree};
-        for(int i =0; i<numberArray.length; i++){
-            if(numberArray[i] > 12 && numberArray[i] < 20){
-                return true;
-            }
-        }
-        return false;
+        System.out.println(area(5, 4));
+        System.out.println(area(-1));
     }
 
-    public static boolean isTeen(int numberOne){
-        return (numberOne > 12 && numberOne < 20);
+    /*
+    Write a method named area with one double parameter named radius.
+    The method needs to return a double value that represents the area of a circle.
+    If the parameter radius is negative then return -1.0 to represent an invalid value.
+
+    Write another overloaded method with 2 parameters x and y (both doubles), where x and y represent the sides of a rectangle.
+    The method needs to return an area of a rectangle.
+
+    If either or both parameters is/are a negative return -1.0 to indicate an invalid value.
+    */
+
+    public static double area(double radius) {
+        return (radius < 0 ? -1 : radius * radius * Math.PI);
+    }
+
+    public static double area(double x, double y) {
+        return (x < 0 || y < 0 ? -1 : x * y);
     }
 }
